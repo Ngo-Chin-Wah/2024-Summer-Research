79883aff73f9ef6a3b86f53bddb5db0a4ffe3493
Nathan Ngo <ngonathan@gmail.com>
2024-05-28 16:30:07 -0600

    Commit

--- Cholesky Decomposition.py
+++ Cholesky Decomposition.py
@@ -1,26 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
+global m, b, t0, tf, h
 m = 0.1 * 10 ** (-6)
 b = 0.2 * 10 ** (-6)
 t0 = 0.0 
 tf = 10.0
-h = 0.01
-desired_std = np.sqrt(np.sqrt(2 * m * b ** 2))
+h = 0.001
 
-time_matrix = h * np.abs(np.arange((tf - t0) / h)[:, None] - np.arange((tf - t0) / h)[None, :])
+def noise():
+    time_matrix = h * np.abs(np.arange((tf - t0) / h)[:, None] - np.arange((tf - t0) / h)[None, :])
+    covariance_matrix = np.zeros_like(time_matrix)
 
-covariance_matrix = np.zeros_like(time_matrix)
+    for i in range(len(time_matrix)):
+        for j in range(len(time_matrix)):
+            covariance_matrix[i][j] = (2 * m) ** 0.5 * b * np.exp(- m * time_matrix[i][j] * 0.5)
+            if (i == j):
+                covariance_matrix[i][j] = 1
 
-for i in range(len(time_matrix)):
-    for j in range(len(time_matrix)):
-        covariance_matrix[i][j] = (2 * m) ** 0.5 * b * np.exp(- m * time_matrix[i][j] * 0.5)
-        if (i == j):
-            covariance_matrix[i][j] = 1
+    L = np.linalg.cholesky(covariance_matrix)
+    noise = np.dot(L, np.random.normal(loc = 0, scale = np.sqrt(np.sqrt(2 * m * b ** 2)), size = int((tf - t0) / h)))
+    return noise
 
-L = np.linalg.cholesky(covariance_matrix)
-
-noise = np.dot(L, np.random.normal(loc = 0, scale = np.sqrt(np.sqrt(2 * m * b ** 2)), size = int((tf - t0) / h)))
+noise = noise()
 t_values = np.linspace(t0, tf, int((tf - t0) / h))
 
 plt.plot(t_values, noise)
@@ -32,6 +34,5 @@ plt.savefig('noise.png')
 
 mean = np.mean(noise)
 variance = np.var(noise)
-print("Noise:", noise)
 print("Mean:", mean)
 print("Variance:", variance)
