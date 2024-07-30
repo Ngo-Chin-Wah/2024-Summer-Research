5929dbabdf02a1517e85173702683996be40e7b1
Nathan Ngo <ngonathan@gmail.com>
2024-07-30 15:49:09 -0600

    Commit

--- T2 Phase Shift Modified.py
+++ T2 Phase Shift Modified.py
@@ -1,63 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jul 20 17:45:19 2024
+Created on Tue Jul 30 13:00:23 2024
 
 @author: nathanngo
 """
-
 # %%
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy.fft import fft, fftfreq
 
-
 def F(noise_f_temp, tau):
-    """Summary
-    
-    Args:
-        noise_f_temp (TYPE): Description
-        tau (TYPE): Description
-    
-    Returns:
-        TYPE: Description
-    """
     return F0 * np.cos(omega_f * tau + noisiness_f * noise_f_temp)
 
-
 def f(noise_f_temp, noise_temp, tau, S):
-    """Summary
-    
-    Args:
-        noise_f_temp (TYPE): Description
-        noise_temp (TYPE): Description
-        tau (TYPE): Description
-        S (TYPE): Description
-    
-    Returns:
-        TYPE: Description
-    """
     dSdt = np.zeros_like(S)
     dSdt[0] = S[1]
     dSdt[1] = F(noise_f_temp, tau) - 2 * zeta * S[1] - S[0] - noisiness * noise_temp
     return dSdt
 
-
 def RK45(f, tau0, tauf, S0, h):
-    """Summary
-    
-    Args:
-        f (TYPE): Description
-        tau0 (TYPE): Description
-        tauf (TYPE): Description
-        S0 (TYPE): Description
-        h (TYPE): Description
-    
-    Returns:
-        TYPE: Description
-    """
     tau_values = np.array([tau0])
     x_values = np.array([[S0[0], S0[1]]])
     tau = tau0
@@ -65,11 +28,18 @@ def RK45(f, tau0, tauf, S0, h):
     noise_iso = np.empty(0, dtype=float)
     noise_f_iso = np.empty(0, dtype=float)
 
+    global cycle_count
+    cycle_count = 0
+    noise_f_temp = 0
+
     while tau < tauf:
         noise_temp = np.random.normal(loc=0, scale=1)
         noise_iso = np.append(noise_iso, noise_temp)
-        noise_f_temp = np.random.normal(loc=0, scale=1)
-        noise_f_iso = np.append(noise_f_iso, noise_f_temp)
+        if cycle_count >= 3:
+            noise_f_temp += np.random.normal(loc=2 * np.pi, scale=1)  # New random phase shift
+            cycle_count = 0  # Reset cycle count
+            noise_f_iso = np.append(noise_f_iso, noisiness_f * noise_f_temp)
+            print(tau, noisiness_f * noise_f_temp)
         n = n + 1
         x = x_values[-1, :]
         k1 = h * f(noise_f_temp, noise_temp, tau, x)
@@ -89,7 +59,7 @@ def RK45(f, tau0, tauf, S0, h):
             (28561 / 56430) * k4 - (9 / 50) * k5 + (2 / 55) * k6
         error = abs(z_new[0] - x_new[0])
         s = 0.84 * (error_m / error) ** (1 / 4)
-        print(tau, h)
+        # print(tau, h)
 
         while (error > error_m):
             h = s * h
@@ -110,20 +80,24 @@ def RK45(f, tau0, tauf, S0, h):
                 (28561 / 56430) * k4 - (9 / 50) * k5 + (2 / 55) * k6
             error = abs(z_new[0] - x_new[0])
             s = (error_m / error) ** (1 / 5)
-            print(tau, h)
+            # print(tau, h)
 
         x_values = np.concatenate((x_values, [x_new]), axis=0)
         tau_values = np.append(tau_values, tau + h)
         tau = tau + h
+
+        cycle_count += (omega_f * h) / (2 * np.pi)
+
     return tau_values, x_values, noise_iso
+
 # %%
 
-global error_m, omega_f, F0, h_interpolate, noisiness, noisiness_f
+global error_m, omega_f, F0, h_interpolate, noisiness, noisiness_f, phase_shift, cycle_count
 
 x0 = 0.0
 v0 = 0.0
 tau0 = 0.0
-tauf = 100.0
+tauf = 125.0
 S0 = np.array([x0, v0])
 
 h = 0.1
@@ -133,74 +107,139 @@ zeta = 0.0
 
 F0 = 1
 noisiness = 0
-noisiness_f = 0
-# %%
-omega_f = 1
 
-t_values_spline = np.arange(tau0, tauf, h_interpolate)
+omega_f = 1.0
+cycle_count = 0
+# %%
+noisiness_f = 0
 
 t_values_temp, x_values_temp, noise_iso = RK45(f, tau0, tauf, S0, h)
+
+t_values_spline = np.arange(tau0, tauf, h_interpolate)
 interpolator = interp1d(t_values_temp, x_values_temp[:, 0], kind='cubic')
 x_values_spline = interpolator(t_values_spline)
-
-plt.plot(t_values_spline, x_values_spline)
-plt.show()
+interpolator = interp1d(t_values_temp, x_values_temp[:, 1], kind='cubic')
+v_values_spline = interpolator(t_values_spline)
 
 X = fft(x_values_spline)
 freqs = fftfreq(len(t_values_spline), h_interpolate)
+X_noiseless = np.abs(X[freqs >= 0])
+freqs_noiseless = freqs[freqs >= 0]
 
-X = np.abs(X[freqs >= 0])
-freqs = freqs[freqs >= 0]
-
-plt.plot(freqs[:30], X[:30])
+plt.plot(freqs_noiseless[:30], X_noiseless[:30], label='Noiseless')
 plt.xlabel(r'Frequency', usetex=True)
 plt.ylabel(r'Amplitude', usetex=True)
 plt.title(r'Fourier Transform; Underdamped; Noisy Force; Detuned to 0 Linewidth', usetex=True)
 plt.grid(True)
+plt.legend()
 plt.savefig('Relaxation Decoherence FFT.pdf')
 plt.show()
 
-# %%
-omega_f = 1 + 10 * 0.055999999999999994
-
-t_values_spline = np.arange(tau0, tauf, h_interpolate)
+noisiness_f = 10
 
 t_values_temp, x_values_temp, noise_iso = RK45(f, tau0, tauf, S0, h)
+
+t_values_spline = np.arange(tau0, tauf, h_interpolate)
 interpolator = interp1d(t_values_temp, x_values_temp[:, 0], kind='cubic')
 x_values_spline = interpolator(t_values_spline)
+interpolator = interp1d(t_values_temp, x_values_temp[:, 1], kind='cubic')
+v_values_spline = interpolator(t_values_spline)
 
 X = fft(x_values_spline)
 freqs = fftfreq(len(t_values_spline), h_interpolate)
+X_noisy = np.abs(X[freqs >= 0])
+freqs_noisy = freqs[freqs >= 0]
 
-X = np.abs(X[freqs >= 0])
-freqs = freqs[freqs >= 0]
-
-plt.plot(freqs[:100], X[:100])
+plt.plot(freqs_noiseless[10:30], X_noiseless[10:30], label='Noiseless')
+plt.plot(freqs_noisy[10:30], X_noisy[10:30], label='Noisy')
 plt.xlabel(r'Frequency', usetex=True)
 plt.ylabel(r'Amplitude', usetex=True)
-plt.title(r'Fourier Transform; Underdamped; Noisy Force; Detuned to 2 Linewidth', usetex=True)
+plt.title(r'Fourier Transform; Undamped; Detuned to 0 Linewidth', usetex=True)
 plt.grid(True)
+plt.legend()
 plt.savefig('Relaxation Decoherence FFT.pdf')
 plt.show()
-
 # %%
-freqs_new = freqs[:30]
-abs_new = np.abs(X)[:30]
-freqs_spline = np.arange(np.min(freqs_new), np.max(freqs_new), 0.001)
 
-interpolator = interp1d(freqs_new, abs_new, kind='cubic')
-abs_spline = interpolator(freqs_spline)
+omega_f = 1 + 0.015000000000000013
+noisiness_f = 0
 
-plt.plot(freqs_spline, abs_spline)
+t_values_temp, x_values_temp, noise_iso = RK45(f, tau0, tauf, S0, h)
+
+t_values_spline = np.arange(tau0, tauf, h_interpolate)
+interpolator = interp1d(t_values_temp, x_values_temp[:, 0], kind='cubic')
+x_values_spline = interpolator(t_values_spline)
+interpolator = interp1d(t_values_temp, x_values_temp[:, 1], kind='cubic')
+v_values_spline = interpolator(t_values_spline)
+
+X = fft(x_values_spline)
+freqs = fftfreq(len(t_values_spline), h_interpolate)
+X_noiseless = np.abs(X[freqs >= 0])
+freqs_noiseless = freqs[freqs >= 0]
+
+noisiness_f = 10
+
+t_values_temp, x_values_temp, noise_iso = RK45(f, tau0, tauf, S0, h)
+
+t_values_spline = np.arange(tau0, tauf, h_interpolate)
+interpolator = interp1d(t_values_temp, x_values_temp[:, 0], kind='cubic')
+x_values_spline = interpolator(t_values_spline)
+interpolator = interp1d(t_values_temp, x_values_temp[:, 1], kind='cubic')
+v_values_spline = interpolator(t_values_spline)
+
+X = fft(x_values_spline)
+freqs = fftfreq(len(t_values_spline), h_interpolate)
+X_noisy = np.abs(X[freqs >= 0])
+freqs_noisy = freqs[freqs >= 0]
+
+plt.plot(freqs_noiseless[10:30], X_noiseless[10:30], label='Noiseless')
+plt.plot(freqs_noisy[10:30], X_noisy[10:30], label='Noisy')
+plt.xlabel(r'Frequency', usetex=True)
+plt.ylabel(r'Amplitude', usetex=True)
+plt.title(r'Fourier Transform; Undamped; Detuned to 1 Linewidth', usetex=True)
+plt.grid(True)
+plt.legend()
+plt.savefig('Relaxation Decoherence FFT.pdf')
 plt.show()
+# %%
 
-abs_max = np.max(abs_spline)
-freqs_above = np.empty(0, dtype=float)
+omega_f = 1 + 2 * 0.015000000000000013
+noisiness_f = 0
 
-for i in range(len(freqs_spline)):
-    if (abs_spline[i] >= 0.5 * abs_max):
-        print(freqs_spline[i])
-        freqs_above = np.append(freqs_above, freqs_spline[i])
+t_values_temp, x_values_temp, noise_iso = RK45(f, tau0, tauf, S0, h)
 
-T2 = freqs_above[-1] - freqs_above[0]
-print('T2:', T2)
+t_values_spline = np.arange(tau0, tauf, h_interpolate)
+interpolator = interp1d(t_values_temp, x_values_temp[:, 0], kind='cubic')
+x_values_spline = interpolator(t_values_spline)
+interpolator = interp1d(t_values_temp, x_values_temp[:, 1], kind='cubic')
+v_values_spline = interpolator(t_values_spline)
+
+X = fft(x_values_spline)
+freqs = fftfreq(len(t_values_spline), h_interpolate)
+X_noiseless = np.abs(X[freqs >= 0])
+freqs_noiseless = freqs[freqs >= 0]
+
+noisiness_f = 10
+
+t_values_temp, x_values_temp, noise_iso = RK45(f, tau0, tauf, S0, h)
+
+t_values_spline = np.arange(tau0, tauf, h_interpolate)
+interpolator = interp1d(t_values_temp, x_values_temp[:, 0], kind='cubic')
+x_values_spline = interpolator(t_values_spline)
+interpolator = interp1d(t_values_temp, x_values_temp[:, 1], kind='cubic')
+v_values_spline = interpolator(t_values_spline)
+
+X = fft(x_values_spline)
+freqs = fftfreq(len(t_values_spline), h_interpolate)
+X_noisy = np.abs(X[freqs >= 0])
+freqs_noisy = freqs[freqs >= 0]
+
+plt.plot(freqs_noiseless[10:30], X_noiseless[10:30], label='Noiseless')
+plt.plot(freqs_noisy[10:30], X_noisy[10:30], label='Noisy')
+plt.xlabel(r'Frequency', usetex=True)
+plt.ylabel(r'Amplitude', usetex=True)
+plt.title(r'Fourier Transform; Undamped; Detuned to 2 Linewidth', usetex=True)
+plt.grid(True)
+plt.legend()
+plt.savefig('Relaxation Decoherence FFT.pdf')
+plt.show()
